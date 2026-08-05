8b15c92c7fd72a465bf0154f6e1710a266fbf190
rupeshkumarp <2003kumarrupesh@gmail.com>
2026-08-05 20:43:39 +0530

    pushing praticed query

--- sub_query.sql
+++ sub_query.sql
@@ -39,11 +39,11 @@ select*from movies where release_year>some(select min(release_year) from movies)
 
 select *from movies where imdb_rating>all(select avg(imdb_rating) from movies where studio='marvel studios');
 
+
 -- common table expression CTE
 with actor_age (actor_name,age)as (select name , year(curdate())-birth_year from actors)
 select actor_name,age from actor_age where age>70 and age<85;
-
-#movies that produced 500% profit and their rating was less then avg rating from all movies
+-- movies that produced 500% profit and their rating was less then avg rating from all movies
 with per_profit as (
 	select m.title,m.imdb_rating,(f.revenue-f.budget)*100/f.budget as ptc_profit
     from movies m join financials f on m.movie_id=f.movie_id

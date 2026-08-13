6f0093e60fae97c256727bfef10353f32bea38ce
rupeshkumarp <2003kumarrupesh@gmail.com>
2026-08-13 20:57:12 +0530

    update notebook

---
+++ atliq_DB_analysis.sql
@@ -0,0 +1,9 @@
+SELECT * FROM dim_customer;
+select distinct market from dim_customer;
+select distinct channel from dim_customer;
+select distinct region from dim_customer;
+
+select*from dim_product;
+select distinct division from dim_product;
+select distinct segment from dim_product;
+select distinct category from dim_product;

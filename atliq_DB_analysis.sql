56c11a12960f7fc3795ac5c8b66ead8a036b5e54
rupeshkumarp <2003kumarrupesh@gmail.com>
2026-08-14 22:21:28 +0530

    update notebook

--- atliq_DB_analysis.sql
+++ atliq_DB_analysis.sql
@@ -30,3 +30,24 @@ year(date_add(date ,interval 4 month))=2021;
 select*from fact_sales_monthly 
 where customer_code=90002002 and
 get_financial_year(date)=2020;
+
+
+-- CREATE DEFINER=`root`@`localhost` FUNCTION `get_financial_q`(
+-- calender_date date) RETURNS char(2) CHARSET utf8mb4
+--     DETERMINISTIC
+-- BEGIN
+-- declare m tinyint;
+-- declare qtr char(2);
+-- set m=month(calender_date);
+--       case 
+-- 		when m in (9,10,11)then set qtr='q1';
+--         when m in (12,1,2)then set qtr='q2';
+--         when m in (3,4,5)then set qtr='q3';
+-- 		else set qtr='q4';
+-- 	end case;
+--     return qtr;
+-- END
+-- selecting q4 sales info of customer_code 90002002
+select*from fact_sales_monthly where customer_code=90002002 and 
+get_financial_q(date)='q4';
+

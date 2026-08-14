7a4f3b4a4104786d64cdf0276b93f4370d397da5
rupeshkumarp <2003kumarrupesh@gmail.com>
2026-08-14 21:24:08 +0530

    update notebook

--- atliq_DB_analysis.sql
+++ atliq_DB_analysis.sql
@@ -7,3 +7,26 @@ select*from dim_product;
 select distinct division from dim_product;
 select distinct segment from dim_product;
 select distinct category from dim_product;
+
+-- finding customer code
+select*from dim_customer where customer like "%croma%";
+
+select sum(sold_quantity),product_code from fact_sales_monthly where customer_code=90002002 group by product_code;
+
+-- selecting sales of customer_code for financial year 2025
+select *from fact_sales_monthly 
+where customer_code=90002002 and
+year(date_add(date ,interval 4 month))=2021;
+
+-- CREATE DEFINER=`root`@`localhost` FUNCTION `get_fiscal_year`(calender_date date) RETURNS int
+--     DETERMINISTIC
+-- BEGIN
+-- 	declare fiscal_year int;
+--     set fiscal_year=year(date_add(cleander_date ,interval 4 month));
+-- 		RETURN fiscal_year;
+-- END
+
+-- using declared variable  
+select*from fact_sales_monthly 
+where customer_code=90002002 and
+get_financial_year(date)=2020;

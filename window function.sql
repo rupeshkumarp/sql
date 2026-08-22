d51a2ecc45e95466987ed1b2fd45734deb231065
rupeshkumarp <2003kumarrupesh@gmail.com>
2026-08-22 19:57:46 +0530

    window function

--- window function.sql
+++ window function.sql
@@ -26,7 +26,29 @@ n.customer_code=dc.customer_code
 WHERE n.fiscal_year = 2021
 GROUP BY dc.customer
 order by net_sales_mln)
-select *,sum(net_sales_mln) over() from  cte1
+select *,sum(net_sales_mln) over() from  cte1;
 
 
 
+
+-- Calculate each customer's net sales and their percentage contribution to the total sales of their region.
+with cte2 as(
+select
+	dc.customer,
+	dc.region,
+    sum(net_sales) as net_sales
+FROM net_sale n join
+dim_customer dc on
+n.customer_code=dc.customer_code 
+WHERE n.fiscal_year = 2021
+GROUP BY dc.region, dc.customer
+)
+select 
+	customer,
+    region,
+    ROUND(net_sales/ 1000000, 2) AS net_sales_mln,
+    ROUND(
+        net_sales * 100 /
+        SUM(net_sales) OVER (PARTITION BY region),2) AS plt
+from  cte2 
+order by region , net_sales_mln desc;

466a5c454babdaeefa6cd773714306d6ac4a8397
rupeshkumarp <2003kumarrupesh@gmail.com>
2026-08-25 19:56:18 +0530

    feature engineering

--- feature engineering.sql
+++ feature engineering.sql
@@ -36,6 +36,74 @@ where sold_quantity is null;
 
 
 -- changing null values to 0 in column forecast_quantity
-update fact_act_est 
-set forecast_quantity=0
-where forecast_quantity is null
+-- update fact_act_est 
+-- set forecast_quantity=0
+-- where forecast_quantity is null
+
+-- -- trigger creation
+-- DROP TRIGGER IF EXISTS `gdb0041`.`fact_act_est_AFTER_INSERT`;
+
+-- DELIMITER $$
+-- USE `gdb0041`$$
+-- CREATE DEFINER = CURRENT_USER TRIGGER `gdb0041`.`fact_act_est_AFTER_INSERT` AFTER INSERT ON `fact_act_est` FOR EACH ROW
+-- BEGIN
+-- 	insert into fact_act_est
+-- 		(date,product_code,customer_code,sold_quantity)
+--     values(
+-- 		new.date,
+--         new.product_code,
+-- 		new.customer_code,
+--         new.sold_quantity
+-- 	)
+--     on duplicate key update
+--     sold_quantity=values(sold_quantity);
+-- END$$
+-- DELIMITER ;
+
+-- display trigger
+show triggers;
+
+insert into fact_sales_monthly(
+	date,product_code,customer_code,sold_quantity)
+values('2025-09-01','ram',2003,28);
+select*from fact_sales_monthly where customer_code=2003;
+
+select*from fact_act_est where customer_code=2003;-- trigger is working fine,here forecast_quantity is null
+-- drop TRIGGER trigger_name;
+
+-- created trigger in forecast_monthly table
+insert into fact_forecast_monthly
+(date,product_code,customer_code,forecast_quantity)
+values('2025-09-01','ram',2003,43);
+
+select*from fact_act_est where customer_code=2003;
+
+
+use random_table;
+CREATE TABLE random_tables.session_logs (ts DATETIME, session_id INT, user_id INT, log TEXT);
+
+INSERT INTO random_tables.session_logs  (ts, session_id, user_id, log) 
+VALUES 
+('2022-10-04 08:14:07', '898812', '523', 'CLICKED | Courses Buttom'),
+('2022-10-14 08:18:35', '898812', '523', 'NAVIAGE BACK | Python course page , codebasics.io'),
+('2022-10-16 12:07:00', '965345', '523', 'REVIEW GENERATED | Data analytics in power bi'),
+('2022-10-22 14:09:22', '188567', '707', 'NEW LOGIN | New login, user name: tasty@jalebi.com'),
+('2022-10-22 18:10:06', '188567', '707', 'COURSE PURCHASED | Data analytics in power bi, user name: tasty@jalebi.com');
+
+select*from session_logs;
+
+show variables like '%event%';
+
+-- creating event
+delimiter $
+create event e_daily_log_purge
+on schedule 
+	every 5 second
+    comment 'purge logs that are 5 days or older'
+do begin
+delete from session_logs
+where date(ts)<date('2021-10-18')-interval 5 day;
+end $
+delimiter ;
+
+show events;
